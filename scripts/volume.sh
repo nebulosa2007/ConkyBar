b758e9c17b3066951284c1e922ce93ffd779afab
nebulosa <nebulosa2007@yandex.ru>
2021-07-10 10:14:48 +0300

    change bash env

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #amixer get Master | awk -F'[]%[]' '/%/ {if ($5 == "off") { print "Muted";exit } else { print $2"%";exit }}'
 mpc | sed 's/:/ /g' | awk '/volume/ {if ($2 == "0%") { print "Muted";exit } else { print $2;exit }}'
